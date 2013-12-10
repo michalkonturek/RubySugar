471cdb22cb41284c2ea115764730ca84fcc84123
Michal Konturek <michal.konturek@gmail.com>
2013-12-10 18:20:28 +0000

    Initial project.

---
+++ RubySugar/RubySugar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RubySugar
+//
+//  Created by Michal Konturek on 10/12/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
