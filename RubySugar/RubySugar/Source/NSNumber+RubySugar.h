5b1aa6cd87e255c0791f81a4989b3b44abfb7ce9
Michal Konturek <michal.konturek@gmail.com>
2013-12-18 11:35:06 +0000

    .

---
+++ RubySugar/RubySugar/Source/NSNumber+RubySugar.h
@@ -0,0 +1,13 @@
+//
+//  NSNumber+RubySugar.h
+//  RubySugar
+//
+//  Created by Michal Konturek on 18/12/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (RubySugar)
+
+@end
---
+++ RubySugar/RubySugarTests/NSNumber+RubySugar_Tests.m
@@ -0,0 +1,32 @@
+//
+//  NSNumber+RubySugar_Tests.m
+//  RubySugar
+//
+//  Created by Michal Konturek on 18/12/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#import "NSNumber+RubySugar.h"
+
+@interface NSNumber_RubySugar_Tests : XCTestCase
+
+@end
+
+@implementation NSNumber_RubySugar_Tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+
+
+@end
