693fc521a1b83a94e4925c614684c901f2bc6afb
Michal Konturek <michal.konturek@gmail.com>
2013-12-24 01:03:53 +0000

    .

---
+++ RubySugar/RubySugarTests/NSArray+RubySugar_Tests.m
@@ -0,0 +1,19 @@
+//
+//  NSArray+RubySugar_Tests.m
+//  RubySugar
+//
+//  Created by Michal Konturek on 24/12/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSArray_RubySugar_Tests : XCTestCase
+
+@end
+
+@implementation NSArray_RubySugar_Tests
+
+
+
+@end
