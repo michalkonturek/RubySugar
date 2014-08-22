0e579b7665115f8a63b1d5c10a7553c76d509af0
Michal Konturek <michal.konturek@gmail.com>
2014-08-22 11:24:13 +0100

    .

---
+++ Source/NSObject+RubySugar.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+RubySugar.h
+//  RubySugar
+//
+//  Created by Michal Konturek on 22/08/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (RubySugar)
+
+@end
---
+++ Source/NSObject+RubySugar.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+RubySugar.m
+//  RubySugar
+//
+//  Created by Michal Konturek on 22/08/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "NSObject+RubySugar.h"
+
+@implementation NSObject (RubySugar)
+
+@end
